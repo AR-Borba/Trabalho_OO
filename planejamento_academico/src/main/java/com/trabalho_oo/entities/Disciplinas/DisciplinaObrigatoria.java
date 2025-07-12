1031e24c6079880600c5eaa0eb726523b990a1a8
Arthur <arthurborba40@gmail.com>
2025-07-12 18:45:03 -0300

    implementa construtores disciplinas

--- planejamento_academico/src/main/java/com/trabalho_oo/entities/Disciplinas/DisciplinaObrigatoria.java
+++ planejamento_academico/src/main/java/com/trabalho_oo/entities/Disciplinas/DisciplinaObrigatoria.java
@@ -1,5 +1,12 @@
 package com.trabalho_oo.entities.Disciplinas;
 
-public class DisciplinaObrigatoria extends Disciplina {
+import java.util.ArrayList;
 
+import com.trabalho_oo.Models.CodigoDisciplina;
+import com.trabalho_oo.Validadores.ValidadorPreRequisito;
+
+public class DisciplinaObrigatoria extends Disciplina {
+    public DisciplinaObrigatoria(String nomeDisciplina, CodigoDisciplina codigo, int cargaHorariaSemanal, ArrayList<ValidadorPreRequisito> preRequisitos, ArrayList<Disciplina> coRequisitos) {
+        super(nomeDisciplina, codigo, cargaHorariaSemanal, preRequisitos, coRequisitos);
+    }
 }
--- planejamento_academico/src/main/java/com/trabalho_oo/entities/Disciplinas/DisciplinaOptativa.java
+++ planejamento_academico/src/main/java/com/trabalho_oo/entities/Disciplinas/DisciplinaOptativa.java
@@ -1,5 +1,12 @@
 package com.trabalho_oo.entities.Disciplinas;
 
+import java.util.ArrayList;
+
+import com.trabalho_oo.Models.CodigoDisciplina;
+import com.trabalho_oo.Validadores.ValidadorPreRequisito;
+
 public class DisciplinaOptativa extends Disciplina {
-    
+    public DisciplinaOptativa(String nomeDisciplina, CodigoDisciplina codigo, int cargaHorariaSemanal, ArrayList<ValidadorPreRequisito> preRequisitos, ArrayList<Disciplina> coRequisitos) {
+        super(nomeDisciplina, codigo, cargaHorariaSemanal, preRequisitos, coRequisitos);
+    }
 }
