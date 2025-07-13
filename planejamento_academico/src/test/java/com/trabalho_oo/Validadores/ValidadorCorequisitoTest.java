50f3a6cd126d4363993325da491e13e465e7ccb0
Arthur <arthurborba40@gmail.com>
2025-07-13 18:58:22 -0300

    atualiza Teste de validadores

--- planejamento_academico/src/test/java/com/trabalho_oo/Validadores/ValidadorCorequisitoTest.java
+++ planejamento_academico/src/test/java/com/trabalho_oo/Validadores/ValidadorCorequisitoTest.java
@@ -3,7 +3,7 @@ package com.trabalho_oo.Validadores;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,38 +30,37 @@ public class ValidadorCorequisitoTest {
     private Turma algIIsalaTurma;
     private Turma algIIpraticaTurma;
     private DiaDaSemana diaSemana = DiaDaSemana.SEGUNDA;
-    private LocalDateTime horarioInicio;
+    private LocalTime horarioInicio;
 
     @BeforeEach
     void setUp() {
-        aluno = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
-        algIIsala = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
-        algIIpratica = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
-        algIIsalaTurma = new Turma('A', 30, 0, diaSemana, "08:00", "10:00", "Sala 101", algIIsala);
-        algIIpraticaTurma = new Turma(algIIpratica, "Segunda", "10:00", "12:00", 30);
-        gradeFutura = new ArrayList<>();
+        aluno             = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
+        algIIsala         = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
+        algIIpratica      = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
+        algIIsalaTurma    = new Turma('A', 30, 0, diaSemana, horarioInicio, horarioInicio, "Sala 101", algIIsala);
+        algIIpraticaTurma = new Turma('A', 30, 0, diaSemana, horarioInicio, horarioInicio, "Sala 101", algIIsala);
+        gradeFutura       = new ArrayList<>();
         gradeFutura.add(algIIsalaTurma);
         gradeFutura.add(algIIpraticaTurma);
     }
 
     @Test
-    void deveRetornarTrueSePreRequisitoFoiAprovado() {
+    void Retorna_True_Para_CoRequisito_Matriculado() {
         aluno.adicionarAoPlanejamento(null);
-        ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(8);
+        ValidadorCorequisito validador = new ValidadorCorequisito(algIIsala);
 
-        boolean resultado = validador.validar(aluno, TCC);
+        boolean resultado = validador.validar(aluno, algIIpratica);
         
         assertTrue(resultado);
     }
 
     @Test
-    void deveRetornarFalseSePreRequisitoFoiReprovado() {
-        aluno.adicionarAoHistorico(calculo1, 70.0);
-        aluno.adicionarAoHistorico(algoritmos, 70.0);
-        ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(20);
+    void Retorna_False_Para_CoRequisito_NAO_Matriculado() {
+        aluno.adicionarAoPlanejamento(null);
+        ValidadorCorequisito validador = new ValidadorCorequisito(algIIsala);
 
-        boolean resultado = validador.validar(aluno, TCC);
-
+        boolean resultado = validador.validar(aluno, algIIpratica);
+        
         assertFalse(resultado);
     }
 }
