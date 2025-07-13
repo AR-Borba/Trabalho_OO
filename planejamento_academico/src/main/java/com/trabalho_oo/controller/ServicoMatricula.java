e1e01ec13491b81b396f52bd9a5414dd95a47fdb
Enzo-Gabriell <enzogrledo@gmail.com>
2025-07-13 19:35:59 -0300

    termina sistema de matricula(error com historico)

--- planejamento_academico/src/main/java/com/trabalho_oo/controller/ServicoMatricula.java
+++ planejamento_academico/src/main/java/com/trabalho_oo/controller/ServicoMatricula.java
@@ -1,24 +1,46 @@
 package com.trabalho_oo.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.trabalho_oo.Validadores.ValidadorPreRequisito;
+import com.trabalho_oo.Validadores.*;
 import com.trabalho_oo.entities.*;
 import com.trabalho_oo.entities.Disciplinas.*;
 import com.trabalho_oo.exceptions.*;
 
 public class ServicoMatricula {
-    
-    // public void matriculaAluno(Aluno aluno) throws TurmaCheiaException {
-    //     for(Turma turmaDesejada : aluno.getGradeFutura()) {
-    //         if(turmaDesejada.isCheia())
-    //             throw new TurmaCheiaException();
-    //         if(turmaDesejada.getDisciplina().getPreRequisitos())
-    // }
 
     public void realizarMatricula(Aluno aluno) {
+        List<Turma> turmasDesejadas = new ArrayList<>(aluno.getGradeFutura());
+        List<Turma> turmasConfirmadas = new ArrayList<>();
+        Map<String, String> disciplinasRejeitadas = new HashMap<>();
+        int cargaHorariaAtual = 0;
+        
+        for(Turma turma : turmasDesejadas) {
+            try {
+                validarPreRequisitos(aluno);
+                validarCoRequisitos(aluno, turma.getDisciplina().getCoRequisitos());
+                verificarConflito(turma, turmasConfirmadas);
+                validarCargaHoraria(aluno, turma.getDisciplina(), cargaHorariaAtual);
+                validarVagas(turma);
+                
+                cargaHorariaAtual += turma.getDisciplina().getCargaHorariaSemanal();
+                turmasConfirmadas.add(turma);
+            } catch (PreRequisitoNaoCumpridoException | CoRequisitoNaoAtendidoException | ConflitoDeHorarioException | CargaHorariaExcedidaException | TurmaCheiaException e) {
+                disciplinasRejeitadas.put(turma.getDisciplina().getNomeDisciplina(), e.getMessage());
+            }
+        }
 
+        System.out.println("IMPRIME RELATÓRIO DE MATRÍCULA");
+        System.out.println("Disciplinas Matriculadas:");
+        for (Turma turma : turmasConfirmadas) 
+            System.out.println("Matriculado em: " + turma.getDisciplina().getNomeDisciplina());
+        System.out.println("Disciplinas Rejeitadas:");
+        for (Map.Entry<String, String> entry : disciplinasRejeitadas.entrySet()) {
+            System.out.println("Disciplina: " + entry.getKey() + " - Motivo: " + entry.getValue());
+        }
     }
 
     private void validarPreRequisitos(Aluno aluno) throws PreRequisitoNaoCumpridoException{
@@ -29,19 +51,34 @@ public class ServicoMatricula {
         }
     }
 
-    // private void validarCoRequisitos(Aluno aluno, List<Disciplina> coRequisitos) throws CoRequisitoNaoAtendidoException {
-    //     if(coRequisitos == null || coRequisitos.isEmpty())
-    //         return;
+    private void validarCoRequisitos(Aluno aluno, List<Disciplina> coRequisitos) throws CoRequisitoNaoAtendidoException {
+        if(coRequisitos == null || coRequisitos.isEmpty())
+            return;
 
-    //     for(Disciplina coRequisito : coRequisitos) {
-    //         if(aluno.getGradeFutura().get)
-    // }
+        for(Disciplina coRequisito : coRequisitos) {
+            ValidadorCorequisito validador = new ValidadorCorequisito(coRequisito);
+            if(!validador.validar(aluno, null))
+                throw new CoRequisitoNaoAtendidoException();
+        }
+    }
 
     private void verificarConflito(Turma turma, List<Turma> turmasMatriculadas) throws ConflitoDeHorarioException {
         for (Turma turmaMatriculada : turmasMatriculadas) {
             if (turmaMatriculada.hasConflict(turma)) {
                 throw new ConflitoDeHorarioException();
             }
+        }
+    }
+
+    private void validarCargaHoraria(Aluno aluno, Disciplina novaDisciplina, int cargaHorariaAtual) throws CargaHorariaExcedidaException {
+        if (cargaHorariaAtual + novaDisciplina.getCargaHorariaSemanal() > aluno.getCargaHorariaMaxima()) {
+            throw new CargaHorariaExcedidaException();
+        }
+    }
+
+    private void validarVagas(Turma turma) throws TurmaCheiaException {
+        if (turma.isCheia()) {
+            throw new TurmaCheiaException();
         }
     }
 }
--- planejamento_academico/src/test/java/com/trabalho_oo/Validadores/ValidadorCorequisitoTest.java
+++ planejamento_academico/src/test/java/com/trabalho_oo/Validadores/ValidadorCorequisitoTest.java
@@ -1,80 +1,80 @@
-package com.trabalho_oo.Validadores;
+// package com.trabalho_oo.Validadores;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+// import java.time.LocalDateTime;
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
-import com.trabalho_oo.Models.DiaDaSemana;
-import com.trabalho_oo.entities.Aluno;
-import com.trabalho_oo.entities.Turma;
-import com.trabalho_oo.entities.Disciplinas.Disciplina;
-import com.trabalho_oo.entities.Disciplinas.DisciplinaObrigatoria;
+// import com.trabalho_oo.Models.DiaDaSemana;
+// import com.trabalho_oo.entities.Aluno;
+// import com.trabalho_oo.entities.Turma;
+// import com.trabalho_oo.entities.Disciplinas.Disciplina;
+// import com.trabalho_oo.entities.Disciplinas.DisciplinaObrigatoria;
 
-public class ValidadorCorequisitoTest {
+// public class ValidadorCorequisitoTest {
 
-    // private Aluno aluno;
-    // private Map<Disciplina, Double> historico = new HashMap<>();
-    // private List<Turma> gradeFutura;
+//     // private Aluno aluno;
+//     // private Map<Disciplina, Double> historico = new HashMap<>();
+//     // private List<Turma> gradeFutura;
 
-<<<<<<< HEAD
-    // private DisciplinaObrigatoria algIIsala;
-    // private DisciplinaObrigatoria algIIpratica;
-    // private ArrayList<ValidadorPreRequisito> vazia = new ArrayList<>();
+// <<<<<<< HEAD
+//     // private DisciplinaObrigatoria algIIsala;
+//     // private DisciplinaObrigatoria algIIpratica;
+//     // private ArrayList<ValidadorPreRequisito> vazia = new ArrayList<>();
     
-    // @BeforeEach
-    // void setUp() {
-    //     aluno = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
-    //     algIIsala = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
-    //     algIIpratica = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
-    // }
-=======
-    private DisciplinaObrigatoria algIIsala;
-    private DisciplinaObrigatoria algIIpratica;
-    private ArrayList<ValidadorPreRequisito> vazia = new ArrayList<>();
-    private Turma algIIsalaTurma;
-    private Turma algIIpraticaTurma;
-    private DiaDaSemana diaSemana = DiaDaSemana.SEGUNDA;
-    private LocalDateTime horarioInicio;
+//     // @BeforeEach
+//     // void setUp() {
+//     //     aluno = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
+//     //     algIIsala = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
+//     //     algIIpratica = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
+//     // }
+// =======
+//     private DisciplinaObrigatoria algIIsala;
+//     private DisciplinaObrigatoria algIIpratica;
+//     private ArrayList<ValidadorPreRequisito> vazia = new ArrayList<>();
+//     private Turma algIIsalaTurma;
+//     private Turma algIIpraticaTurma;
+//     private DiaDaSemana diaSemana = DiaDaSemana.SEGUNDA;
+//     private LocalDateTime horarioInicio;
 
-    @BeforeEach
-    void setUp() {
-        aluno = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
-        algIIsala = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
-        algIIpratica = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
-        algIIsalaTurma = new Turma('A', 30, 0, diaSemana, "08:00", "10:00", "Sala 101", algIIsala);
-        algIIpraticaTurma = new Turma(algIIpratica, "Segunda", "10:00", "12:00", 30);
-        gradeFutura = new ArrayList<>();
-        gradeFutura.add(algIIsalaTurma);
-        gradeFutura.add(algIIpraticaTurma);
-    }
->>>>>>> 594a7e02feed89035ad432ed43507b7e7331bb1f
+//     @BeforeEach
+//     void setUp() {
+//         aluno = new Aluno("Aluno de Teste", "202501001", 70, historico, gradeFutura);
+//         algIIsala = new DisciplinaObrigatoria("Algoritmos II - Sala", "DCC200" ,4, vazia, null);
+//         algIIpratica = new DisciplinaObrigatoria("Algoritmos II - Pratica", "DCC201" ,4, vazia, null);        
+//         algIIsalaTurma = new Turma('A', 30, 0, diaSemana, "08:00", "10:00", "Sala 101", algIIsala);
+//         algIIpraticaTurma = new Turma(algIIpratica, "Segunda", "10:00", "12:00", 30);
+//         gradeFutura = new ArrayList<>();
+//         gradeFutura.add(algIIsalaTurma);
+//         gradeFutura.add(algIIpraticaTurma);
+//     }
+// >>>>>>> 594a7e02feed89035ad432ed43507b7e7331bb1f
 
-    // @Test
-    // void deveRetornarTrueSePreRequisitoFoiAprovado() {
-    //     aluno.adicionarAoPlanejamento(null);
-    //     ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(8);
+//     // @Test
+//     // void deveRetornarTrueSePreRequisitoFoiAprovado() {
+//     //     aluno.adicionarAoPlanejamento(null);
+//     //     ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(8);
 
-    //     boolean resultado = validador.validar(aluno, TCC);
+//     //     boolean resultado = validador.validar(aluno, TCC);
         
-    //     assertTrue(resultado);
-    // }
+//     //     assertTrue(resultado);
+//     // }
 
-    // @Test
-    // void deveRetornarFalseSePreRequisitoFoiReprovado() {
-    //     aluno.adicionarAoHistorico(calculo1, 70.0);
-    //     aluno.adicionarAoHistorico(algoritmos, 70.0);
-    //     ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(20);
+//     // @Test
+//     // void deveRetornarFalseSePreRequisitoFoiReprovado() {
+//     //     aluno.adicionarAoHistorico(calculo1, 70.0);
+//     //     aluno.adicionarAoHistorico(algoritmos, 70.0);
+//     //     ValidadorCreditosMinimos validador = new ValidadorCreditosMinimos(20);
 
-    //     boolean resultado = validador.validar(aluno, TCC);
+//     //     boolean resultado = validador.validar(aluno, TCC);
 
-    //     assertFalse(resultado);
-    // }
-}
+//     //     assertFalse(resultado);
+//     // }
+// }
